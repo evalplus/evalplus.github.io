144c97d5d6859b005e5572394a246314932d27e6
ganler <jaway.liu@gmail.com>
2024-04-23 01:30:40 -0500

    add sample results

--- results/repoqa/README.txt
+++
@@ -1,5 +0,0 @@
-We shall store the scores for each model here and in the html file we plot the leaderboard table by analyzing the results.
-
-A simple hierarchy:
-* Each file name presents the context sizes in "k", e.g., 16k.
-* Within each file, each 
--- results/repoqa/update_json_from_eval_results.py
+++
@@ -1,9 +0,0 @@
-# TODO:
-
-"""
-Step 1: Fetch model outputs from https://github.com/evalplus/repoqa/releases/tag/dev-results
-Step 2: Evaluate these outputs to some high-level metrics (multiple json files)
-Step 3: Summarize these metrics into a single json file for each context size config to simplify the leaderboard plotting
-
-NOTE: Only keep the final json files and ignore other intermediate files to make the repo minimal
-"""
