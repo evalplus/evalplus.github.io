0ba8d22cd7b9938b88a200634a52d9015a1598a8
ganler <jaway.liu@gmail.com>
2024-10-31 14:14:49 -0500

    improve

--- results/evalperf/stats.py
+++ results/evalperf/stats.py
@@ -55,7 +55,7 @@ def compute_win_rates(data: dict, metric: str):
     return win_rates
 
 
-def compute_dps_diff(data: dict, model1: str, model2: str):
+def compute_aligned_dps_pair(data: dict, model1: str, model2: str):
     model1_pass_tasks = set(
         task
         for task, result in data[model1]["eval"].items()
@@ -67,12 +67,9 @@ def compute_dps_diff(data: dict, model1: str, model2: str):
         if result["dps"] is not None
     )
     common_tasks = model1_pass_tasks & model2_pass_tasks
-    dps_diff = 0
-    for task in common_tasks:
-        dps_diff += (
-            data[model1]["eval"][task]["dps"] - data[model2]["eval"][task]["dps"]
-        )
-    return dps_diff / len(common_tasks)
+    return sum(data[model1]["eval"][t]["dps"] for t in common_tasks) / len(
+        common_tasks
+    ), sum(data[model2]["eval"][t]["dps"] for t in common_tasks) / len(common_tasks)
 
 
 if __name__ == "__main__":
@@ -90,16 +87,19 @@ if __name__ == "__main__":
         stats[model_id] = data[model_id]["summary"]
         stats[model_id]["win_rate"] = win_rates[model_id]
 
-    dps_diff = defaultdict(dict)
+    dps_pairs = defaultdict(dict)
     for model_id in data.keys():
         for other_model_id in data.keys():
-            if model_id == other_model_id:
-                dps_diff[model_id][other_model_id] = 0.0
-            elif dps_diff[other_model_id].get(model_id, None) is not None:
-                dps_diff[model_id][other_model_id] = -1 * dps_diff[other_model_id][model_id]
+            if dps_pairs[other_model_id].get(model_id, None) is not None:
+                dps_pairs[model_id][other_model_id] = (
+                    dps_pairs[other_model_id][model_id][1],
+                    dps_pairs[other_model_id][model_id][0],
+                )
             else:
-                dps_diff[model_id][other_model_id] = compute_dps_diff(data, model_id, other_model_id)
-    stats["heatmap_data"] = dps_diff
+                dps_pairs[model_id][other_model_id] = compute_aligned_dps_pair(
+                    data, model_id, other_model_id
+                )
+    stats["heatmap_data"] = dps_pairs
 
     with open(DST_PATH, "w") as f:
         json.dump(stats, f, indent=4)
